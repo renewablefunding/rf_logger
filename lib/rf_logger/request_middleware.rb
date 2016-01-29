faa38700dc53b22cb1f6abd90c33937ccec7bd38
Dustin Zeisler <dustin@zeisler.net>
2016-01-29 12:05:18 -0800

    Decouple from Rails/Rory, replace with retrieving out of the env hash.

--- lib/rf_logger/request_middleware.rb
+++ lib/rf_logger/request_middleware.rb
@@ -1,12 +1,10 @@
 module RfLogger
   class RequestMiddleware
     # @param [Hash] options
-    # @option opts [Hash{:label => :request_method}] :tagged The subject
-    # @option opts [Class] :rack_request_class
+    # @option opts [Hash{:label => "header_name"}] :tagged The subject
     def initialize(app, options={})
       @app                = app
-      @tagged             = options.fetch(:tagged, { request_id: :uuid })
-      @rack_request_class = options.fetch(:rack_request_class){request_class}
+      @tagged             = options.fetch(:tagged, { request_id: "X-Request-Id" })
     end
 
     def call(env)
@@ -17,7 +15,7 @@ module RfLogger
 
     def tagged
       @tagged.each_with_object({}) do |(key, value), hash|
-        hash[key] = request_object.send(value) if request_object.respond_to? value
+        hash[key] = @env[value]
       end
     end
 
@@ -25,21 +23,6 @@ module RfLogger
 
     def set_tagged_thread_var
       (Thread.current[:inheritable_attributes] ||= {})[:rf_logger_request_tags] = tagged
-    end
-
-    def request_object
-      @request_object ||= @rack_request_class.new(@env)
-    end
-
-    def request_class
-      case
-      when defined? ActionDispatch::Request
-        ActionDispatch::Request
-      when defined? Rory::Request
-        Rory::Request
-      else
-        raise ArgumentError, "Unknown framework context - :rack_request_class key needed."
-      end
     end
   end
 end
--- spec/lib/rf_logger/request_middleware_spec.rb
+++ spec/lib/rf_logger/request_middleware_spec.rb
@@ -1,74 +1,35 @@
 require "rf_logger/request_middleware"
 
 RSpec.describe RfLogger::RequestMiddleware do
-  subject { RfLogger::RequestMiddleware.new(*init_arguments).call({}) }
+  subject { RfLogger::RequestMiddleware.new(*init_arguments).call(env) }
+  let(:env) { {} }
   let(:init_arguments) { [->(_) {}] }
   after { Thread.current[:rf_logger_request_id] = nil }
 
-  context "Rails" do
-    before { stub_const("ActionDispatch::Request", double("ActionDispatch::Request", new: double("ActionDispatch::Request#", uuid: "uuid-from_env"))) }
-    context "when env responds to uuid" do
-      it "sets current Thread variable" do
-        subject
-        expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({ :request_id => "uuid-from_env" })
-      end
-    end
-
-    context "from custom_env_key" do
-      let(:init_arguments) { [->(_) {}, tagged: { custom_env_key: "other_request_id" }] }
-      before { stub_const("ActionDispatch::Request", double("ActionDispatch::Request", new: double("ActionDispatch::Request#", other_request_id: "other_request_id"))) }
-
-      it "sets current Thread variable" do
-        subject
-        expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({ :custom_env_key => "other_request_id" })
-      end
+  context "when tagged is missing" do
+    let(:env) { { "X-Request-Id" => "uuid-from_env" } }
+    it "defaults to getting the request_id" do
+      subject
+      expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({ :request_id => "uuid-from_env" })
     end
   end
 
-  context "Rory" do
-    before { stub_const("Rory::Request", double("Rory::Request", new: double("Rory::Request#", uuid: "uuid-from_env"))) }
-    context "when env responds to uuid" do
-      it "sets current Thread variable" do
-        subject
-        expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({ :request_id => "uuid-from_env" })
-      end
-    end
+  context "when tagged option is given that exists" do
+    let(:env) { { "X-OtherHeader" => "other_request_id" } }
+    let(:init_arguments) { [->(_) {}, tagged: { custom_env_key: "X-OtherHeader" }] }
 
-    context "from custom_env_key" do
-      let(:init_arguments) { [->(_) {}, tagged: { custom_env_key: "other_request_id" }] }
-      before { stub_const("ActionDispatch::Request", double("ActionDispatch::Request", new: double("ActionDispatch::Request#", other_request_id: "other_request_id"))) }
-
-      it "sets current Thread variable" do
-        subject
-        expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({ :custom_env_key => "other_request_id" })
-      end
+    it "sets current Thread variable" do
+      subject
+      expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({ :custom_env_key => "other_request_id" })
     end
   end
 
-  context "Other Framework Context" do
-    context "when env responds to uuid" do
-      let(:init_arguments) { [->(_) {}, rack_request_class: double("rack_request_class", new: double("rack_request_class#", uuid: "uuid-from_env"))] }
-      it "sets current Thread variable" do
-        subject
-        expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({ :request_id => "uuid-from_env" })
-      end
-    end
+  context "when tagged option is given that does not exists" do
+    let(:init_arguments) { [->(_) {}, tagged: { custom_env_key: "X-WontFindMe" }] }
 
-    context "from custom_env_key" do
-      let(:init_arguments) { [->(_) {}, tagged: { custom_env_key: "other_request_id" }, rack_request_class: double("rack_request_class", new: double("rack_request_class#", other_request_id: "other_request_id"))] }
-
-      it "sets current Thread variable" do
-        subject
-        expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({ :custom_env_key => "other_request_id" })
-      end
-    end
-
-    context "when rack_request_class is not given" do
-      let(:init_arguments) { [->(_) {}] }
-
-      it "sets current Thread variable" do
-        expect { subject }.to raise_error(ArgumentError)
-      end
+    it "sets current Thread variable" do
+      subject
+      expect(Thread.current[:inheritable_attributes][:rf_logger_request_tags]).to eq({:custom_env_key=>nil})
     end
   end
 end
