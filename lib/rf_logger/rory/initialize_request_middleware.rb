7c0a3b331404722116b255a1e6458f0800fe3283
Dustin Zeisler <dustin@zeisler.net>
2016-02-22 17:01:44 -0800

    Ensure the application instance is present before calling use_middleware

--- lib/rf_logger/rory/initialize_request_middleware.rb
+++ lib/rf_logger/rory/initialize_request_middleware.rb
@@ -1,3 +1,10 @@
 require "rf_logger/request/request_middleware"
 
-Rory.application.use_middleware RfLogger::RequestMiddleware
+module Rory
+  class Application
+    def spin_up
+      Rory.application.use_middleware RfLogger::RequestMiddleware
+      super
+    end
+  end
+end
