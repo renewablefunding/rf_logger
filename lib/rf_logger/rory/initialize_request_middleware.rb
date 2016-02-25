5f3f82387ebeb4c9b4dc83e5225cb6af66253b45
Dustin Zeisler <dustin@zeisler.net>
2016-02-25 13:36:46 -0800

    Use method alias chain to hook in before spin_up in Rory

--- lib/rf_logger/rory/initialize_request_middleware.rb
+++ lib/rf_logger/rory/initialize_request_middleware.rb
@@ -2,9 +2,12 @@ require "rf_logger/request/request_middleware"
 
 module Rory
   class Application
+
+    alias_method :_spin_up, :spin_up
+
     def spin_up
       Rory.application.use_middleware RfLogger::RequestMiddleware
-      super
+      _spin_up
     end
   end
 end
